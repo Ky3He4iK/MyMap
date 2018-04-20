a0d7c377e76d03cf5096a129c4bf261196a4c777
ky3he4ik <misha10311@yandex.ru>
2018-04-20 23:13:31 +0500

    Testing

---
+++ Main.kt
@@ -0,0 +1,11 @@
+val help = """help - show this message
+    add <key> <value> - add new key
+    set <key> <value> - set
+    <key> = <value> -
+    del <key>
+    <key>
+    show <key>
+    stop
+    exit
+    find
+""".trimIndent()
